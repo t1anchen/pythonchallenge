4e3ebcf98e2bfb2cea1f92b66e5205194744482a
au9ustine <duke.augustine@gmail.com>
2015-06-25 00:11:07 +0800

    add level 11

--- pythonchallenge/level_11.py
+++ pythonchallenge/level_11.py
@@ -8,6 +8,7 @@ import os
 import os.path
 import Image
 import ImageDraw
+from StringIO import StringIO
 
 # Default is warning, it's to suppress requests INFO log
 logging.basicConfig(format='%(message)s')
@@ -15,14 +16,14 @@ logging.basicConfig(format='%(message)s')
 
 def solution():
     url = 'http://www.pythonchallenge.com/pc/return/cave.jpg'
-    urllib.urlretrieve(url, 'cave.jpg')
-    image_file = Image.open('cave.jpg')
+    r = requests.get(url, auth=('huge', 'file'))
+    image_file = Image.open(StringIO(r.content))
     new_image = Image.new('RGB', (640, 480), 'black')
     new_image_stroke = ImageDraw.Draw(new_image)
-    for y in range(480):
-        for x in range(640):
+    for y in xrange(480):
+        for x in xrange(640):
             if y % 2 == 0 and x % 2 == 0 or y % 2 == 1 and x % 2 == 1:
-                new_image.putpixel((x, y), image_file.getpixel(x, y))
+                new_image.putpixel((x, y), image_file.getpixel((x, y)))
     new_image.save('cave_edited.jpg')
     return 'evil'
 
@@ -31,6 +32,9 @@ class SolutionTest(unittest.TestCase):
     def setUp(self):
         self.prefix = "http://www.pythonchallenge.com/pc/return/"
         self.suffix = ".html"
+
+    def tearDown(self):
+        os.remove('cave_edited.jpg')
 
     def test_solution(self):
         actual = solution()

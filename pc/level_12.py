def4f6bd63ad0e92bb2bccf3aa9c526103dd9d00
au9ustine <duke.augustine@gmail.com>
2016-06-08 21:23:44 +0800

    add level 12

--- pc/level_12.py
+++ pc/level_12.py
@@ -1,2 +1,33 @@
+import requests
+from itertools import cycle
+import os
+
 def solution():
-    url = 'http://www.pythonchallenge.com/pc/return/evil.html'
+    # evil.html -> evil1.jpg -> 5 decks
+    # evil2.jpg -> not jpg - -.gfx
+    # evil3.jpg -> no more evils
+    url = 'http://www.pythonchallenge.com/pc/return/evil2.gfx'
+    auth_ctx = ('huge', 'file')
+    res = requests.get(url, auth=auth_ctx)
+    # https://www.cs.duke.edu/courses/cps124/fall01/code/gfx_reader/docs/gfx_format.html
+    gfx_stream = res.iter_content()
+    decks = [[] for i in range(5)]
+    i = cycle(range(5))
+    for gfx_byte in gfx_stream:
+        decks[next(i)].append(gfx_byte) # dispatch card to 5 decks
+    for i in range(5):
+        with open('img{0:02d}.gfx'.format(i), 'wb') as f:
+            f.write(b''.join(decks[i])) # Python3 byte string
+    # TODO: get font from gfx image
+    # img00.gfx -> dis
+    # img01.gfx -> pro
+    # img02.gfx -> port
+    # img03.gfx -> ional
+    # img04.gfx -> i̶t̶y̶ (abandoned, strikethrough combining U+0336)
+
+    return 'disproportional'
+
+def clean():
+    filenames = ['img{0:02d}.gfx'.format(i) for i in range(5)]
+    for fn in filenames:
+        os.remove(fn)
--- test/test_go_through.py
+++ test/test_go_through.py
@@ -1,9 +1,12 @@
 import re
 import requests
+from nose.tools import nottest
 import pc.level_00
 import pc.level_01
+import pc.level_12
 
 def_template = 'http://www.pythonchallenge.com/pc/def/{0}.html'
+pc_return_tmpl = 'http://www.pythonchallenge.com/pc/return/{0}'
 
 def test_level_00():
     template = def_template
@@ -12,12 +15,13 @@ def test_level_00():
     except:
         raise
 
-    next_entry = [re.sub(r'(.*)URL=(.*)\.html\"\>', r'\2', line)
-                  for line in r.iter_lines() if re.match(r'.*URL.*', line)]
+    next_entry = [re.sub(r'(.*)URL=(.*)\.html\"\>', r'\2', line.decode()) # Python3 `line' would be bytestring
+                  for line in r.iter_lines() if re.match(r'.*URL.*', line.decode())]
     actual = next_entry[0]
     expected = 'map'
     assert actual == expected
 
+@nottest
 def test_level_01():
     template = def_template
     test_data = '''g fmnc wms bgblr rpylqjyrc gr zw fylb. ''' + \
@@ -37,3 +41,9 @@ def test_level_01():
     actual = pc.level_01.solution()
     expected = 'ocr'
     assert expected == actual
+
+def test_level_12():
+    actual = pc.level_12.solution()
+    expected = 'disproportional'
+    assert expected == actual
+    pc.level_12.clean()

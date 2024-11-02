74a03ddc2ba177203a8e0785ac8f1e81f832da0d
t1anchen <143927+t1anchen@users.noreply.github.com>
2024-11-02 16:07:20 +0100

    refactor(level_03): update level_03 with tested

---
+++ pythonchallenge/test/test_level_03.py
@@ -0,0 +1,32 @@
+import gzip
+import logging
+import re
+from pathlib import Path
+
+import requests
+
+from pc.level_03 import solution
+
+from . import def_template
+
+
+def test_unit():
+    expected = "linkedlist"
+    test_data_path = (Path(__file__) / ".." / "fixture" / "level_03.txt.gz").resolve(
+        strict=True
+    )
+    with gzip.open(test_data_path, mode="rt", encoding="utf_8") as reader:
+        actual = solution(reader.read())
+        assert expected == actual
+
+
+def test_integration():
+    expected = "linkedlist"
+    url = def_template.format("equality")
+    logging.debug(f"Visiting {url}")
+    resp = requests.get(url)
+    matched = re.findall(r"<!--([^>]*)-->", resp.text)
+    assert len(matched) >= 1
+    haystack = matched[0]
+    actual = solution(haystack)
+    assert expected == actual
